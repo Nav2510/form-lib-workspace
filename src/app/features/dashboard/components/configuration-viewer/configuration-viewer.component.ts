d14ef5c15bfc30a6f17b75c04a273622303d365c
Navdeep Singh <navdeep.singh@windmill.ch>
2022-05-09 00:59:41 +0530

    [fixes] Fixed form styles issues

--- src/app/features/dashboard/components/configuration-viewer/configuration-viewer.component.ts
+++ src/app/features/dashboard/components/configuration-viewer/configuration-viewer.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CONFIG } from '../../../../shared/mocks/form.mock';
+
 @Component({
   selector: 'app-configuration-viewer',
   templateUrl: './configuration-viewer.component.html',
   styleUrls: ['./configuration-viewer.component.scss']
 })
 export class ConfigurationViewerComponent implements OnInit {
-
+  CONFIG = CONFIG
   constructor() { }
 
   ngOnInit(): void {
