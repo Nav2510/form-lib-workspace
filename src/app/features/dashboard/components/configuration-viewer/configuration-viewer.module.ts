c08763e1e2f366648118c89b91ae76efaab69d35
Navdeep Singh <navdeep.singh@windmill.ch>
2022-05-09 01:49:10 +0530

    [task] Added json config, fixed styles

--- src/app/features/dashboard/components/configuration-viewer/configuration-viewer.module.ts
+++ src/app/features/dashboard/components/configuration-viewer/configuration-viewer.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ConfigurationViewerComponent } from './configuration-viewer.component';
 
 @NgModule({
