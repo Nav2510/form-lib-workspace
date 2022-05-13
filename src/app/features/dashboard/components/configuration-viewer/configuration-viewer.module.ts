50f397f2878eb7dd56a0e980a12b3916cdcb5fda
Navdeep Singh <navdeep.singh@windmill.ch>
2022-05-13 16:18:33 +0530

    [task] Changed layout

--- src/app/features/dashboard/components/configuration-viewer/configuration-viewer.module.ts
+++ src/app/features/dashboard/components/configuration-viewer/configuration-viewer.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { ConfigurationViewerComponent } from './configuration-viewer.component';
 
+const MATERIAL_MODULES = [MatButtonModule, MatIconModule];
+
 @NgModule({
   declarations: [ConfigurationViewerComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ...MATERIAL_MODULES],
   exports: [ConfigurationViewerComponent],
 })
 export class ConfigurationViewerModule {}
